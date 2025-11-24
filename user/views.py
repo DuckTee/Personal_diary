71b1409483235a5b89c7e2438181a1db01a6444a
Ksenia_Sheveleva <brantova00@gmail.com>
2025-11-24 19:49:19 +0300

    Создание пользователя

--- user/views.py
+++ user/views.py
@@ -1,15 +1,25 @@
-from django.shortcuts import redirect, render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 from django.contrib.auth import login
-from .forms import UserRegistrationForm
+from user.forms import UserRegisterForm
+from user.models import User
 
-def register(request):
-    if request.method == 'POST':
-        form = UserRegistrationForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            login(request, user)
-            return redirect('diary:entry_list')
-    else:
-        form = UserRegistrationForm()
+class RegisterView(CreateView):
+    """
+    Регистрация пользователя
+    """
+    model = User
+    form_class = UserRegisterForm
+    template_name = 'users/register.html'  # Исправлен путь
+    success_url = reverse_lazy('diary:entry_list')  # Редирект в дневник
 
-    return render(request, 'users/register.html', {'form': form})
+    def form_valid(self, form):
+        user = form.save()  # Сохраняем пользователя
+        login(self.request, user)  # Автовход
+        return super().form_valid(form)  # Стандартный редирект на success_url
+
+    def get_context_data(self, **kwargs):
+        """Добавляем заголовок страницы в контекст."""
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Регистрация'
+        return context
